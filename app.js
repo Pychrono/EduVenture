be16910d724b638bc6159ac17e27fbb556a5860b
Chinonso Iwuchukwu <chinonsoiwuchukwu7@gmail.com>
2023-10-26 11:54:04 +0100

    now using vue 2

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const app = Vue.createApp({
-    data() {
-        return{
-            showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
-        }
-    },
-    methods: {
-        toggleshowBooks() {
-            this.showBooks = !this.showBooks
-        }
-    },
-})
-
+new Vue({
+    el: '#vue-app'
+});
 app.mount('#app')
   
