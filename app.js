fbf9e55a86369c9314f69a53191083f8aa819fa3
Chinonso Iwuchukwu <chinonsoiwuchukwu7@gmail.com>
2023-10-29 14:28:50 +0100

    next day

--- app.js
+++ app.js
@@ -1,31 +1,92 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<h1>{{ todo.text }}</h1>'
-  })
-
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun',
-        age:'20',
-        groceryList: [
-            { id: 0, text: 'Vegetables' },
-            { id: 1, text: 'Cheese' },
-            { id: 2, text: 'Whatever else humans are supposed to eat' }
-          ],
-        website: 'https://getbootstrap.com/docs/5.3/examples/sidebars/'
+        classes: classes
     },
-    methods:{
-        greet:function(time){
-           return "Good Morning Cunts" + time + " Im talking to you " + this.name; 
-        },
-        add:function(){
-            this.age++;
-        },
-        subtract:function(){
-            this.age--;
-        }
-    }
+ 
 });
+
+var classes = [
+    {
+        id: 1,
+        title: "English Language",
+        location: "Brent",
+        price: 50,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1455540904194-fc101941273a?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW5nbGlzaHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        id: 2,
+        title: "Football Training",
+        location: "Hendon",
+        price: 70,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1589487391730-58f20eb2c308?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vdGJhbGx8ZW58MHx8MHx8fDA%3D"
+    },
+    {
+        id: 3,
+        title: "Mathematics",
+        location: "Hackney",
+        price: 150,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1613905780946-26b73b6f6e11?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1hdGhzfGVufDB8fDB8fHww"
+    },
+    {
+        id: 4,
+        title: "Music",
+        location: "Hackney",
+        price: 50,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1485579149621-3123dd979885?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8fDB8fHww"
+    },
+    {
+        id: 5,
+        title: "Dance Club",
+        location: "Brent",
+        price: 80,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1519925610903-381054cc2a1c?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGRhbmNlfGVufDB8fDB8fHww"
+    },
+    {
+        id: 6,
+        title: "Chemisty",
+        location: "Brent",
+        price: 50,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1603126857599-f6e157fa2fe6?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlbWlzdHJ5fGVufDB8fDB8fHww"
+    },
+    {
+        id: 7,
+        title: "Data Analysis",
+        location: "Hendon",
+        price: 90,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJ1c2luZXNzfGVufDB8fDB8fHww"
+    },
+    {
+        id: 8,
+        title: "Web Development",
+        location: "Brent",
+        price: 200,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1607799279861-4dd421887fb3?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
+    },
+    {
+        id: 9,
+        title: "Weight Lifting",
+        location: "Hendon",
+        price: 40,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2VpZ2h0JTIwbGlmdGluZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        id: 10,
+        title: "Karate",
+        location: "Karate Town",
+        price: 120,
+        spaces: 5,
+        image: "https://images.unsplash.com/photo-1555597673-b21d5c935865?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2FyYXRlfGVufDB8fDB8fHww"
+    }
+]
 
   
---
+++ tun.js
@@ -0,0 +1,108 @@
+
+console.log('Vue instance initialized')
+new Vue({
+    el: "#app",
+    data() {
+      return {
+        classes: [
+            {
+                id: 1,
+                title: "English Language",
+                location: "Brent",
+                price: 50,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1455540904194-fc101941273a?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW5nbGlzaHxlbnwwfHwwfHx8MA%3D%3D"
+            },
+            {
+                id: 2,
+                title: "Football Training",
+                location: "Hendon",
+                price: 70,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1589487391730-58f20eb2c308?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vdGJhbGx8ZW58MHx8MHx8fDA%3D"
+            },
+            {
+                id: 3,
+                title: "Mathematics",
+                location: "Hackney",
+                price: 150,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1613905780946-26b73b6f6e11?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1hdGhzfGVufDB8fDB8fHww"
+            },
+            {
+                id: 4,
+                title: "Music",
+                location: "Hackney",
+                price: 50,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1485579149621-3123dd979885?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8fDB8fHww"
+            },
+            {
+                id: 5,
+                title: "Dance Club",
+                location: "Brent",
+                price: 80,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1519925610903-381054cc2a1c?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGRhbmNlfGVufDB8fDB8fHww"
+            },
+            {
+                id: 6,
+                title: "Chemisty",
+                location: "Brent",
+                price: 50,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1603126857599-f6e157fa2fe6?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlbWlzdHJ5fGVufDB8fDB8fHww"
+            },
+            {
+                id: 7,
+                title: "Data Analysis",
+                location: "Hendon",
+                price: 90,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJ1c2luZXNzfGVufDB8fDB8fHww"
+            },
+            {
+                id: 8,
+                title: "Web Development",
+                location: "Brent",
+                price: 200,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1607799279861-4dd421887fb3?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
+            },
+            {
+                id: 9,
+                title: "Weight Lifting",
+                location: "Hendon",
+                price: 40,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2VpZ2h0JTIwbGlmdGluZ3xlbnwwfHwwfHx8MA%3D%3D"
+            },
+            {
+                id: 10,
+                title: "Karate",
+                location: "Karate Town",
+                price: 120,
+                spaces: 5,
+                image: "https://images.unsplash.com/photo-1555597673-b21d5c935865?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2FyYXRlfGVufDB8fDB8fHww"
+            }
+        ],
+      }
+    },
+    computed: {
+        filteredItems: function() {
+          const query = this.searchQuery.toLowerCase();
+          return this.classes.filter(item => {
+            return item.title.toLowerCase().includes(query) || item.location.toLowerCase().includes(query);
+          });
+        }
+      },
+      methods: {
+        sortByPrice: function() {
+          this.classes.sort((a, b) => a.price - b.price);
+        },
+        sortBySpaces: function() {
+          this.classes.sort((a, b) => a.spaces - b.spaces);
+        }
+      }
+  });
+  
