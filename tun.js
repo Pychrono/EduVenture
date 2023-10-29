fc7e0cefef7fbd5a7444620dc14900bd71ffd8ba
Chinonso Iwuchukwu <chinonsoiwuchukwu7@gmail.com>
2023-10-29 17:21:08 +0100

    icons

--- tun.js
+++ tun.js
@@ -11,6 +11,7 @@ new Vue({
                 location: "Brent",
                 price: 50,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1455540904194-fc101941273a?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW5nbGlzaHxlbnwwfHwwfHx8MA%3D%3D"
             },
             {
@@ -19,6 +20,7 @@ new Vue({
                 location: "Hendon",
                 price: 70,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1589487391730-58f20eb2c308?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vdGJhbGx8ZW58MHx8MHx8fDA%3D"
             },
             {
@@ -27,6 +29,7 @@ new Vue({
                 location: "Hackney",
                 price: 150,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1613905780946-26b73b6f6e11?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1hdGhzfGVufDB8fDB8fHww"
             },
             {
@@ -35,6 +38,7 @@ new Vue({
                 location: "Hackney",
                 price: 50,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1485579149621-3123dd979885?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8fDB8fHww"
             },
             {
@@ -43,6 +47,7 @@ new Vue({
                 location: "Brent",
                 price: 80,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1519925610903-381054cc2a1c?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGRhbmNlfGVufDB8fDB8fHww"
             },
             {
@@ -51,6 +56,7 @@ new Vue({
                 location: "Brent",
                 price: 50,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1603126857599-f6e157fa2fe6?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlbWlzdHJ5fGVufDB8fDB8fHww"
             },
             {
@@ -59,6 +65,7 @@ new Vue({
                 location: "Hendon",
                 price: 90,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJ1c2luZXNzfGVufDB8fDB8fHww"
             },
             {
@@ -67,6 +74,7 @@ new Vue({
                 location: "Brent",
                 price: 200,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1607799279861-4dd421887fb3?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
             },
             {
@@ -75,6 +83,7 @@ new Vue({
                 location: "Hendon",
                 price: 40,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2VpZ2h0JTIwbGlmdGluZ3xlbnwwfHwwfHx8MA%3D%3D"
             },
             {
@@ -83,9 +92,11 @@ new Vue({
                 location: "Karate Town",
                 price: 120,
                 spaces: 5,
+                availableSpaces: 5,
                 image: "https://images.unsplash.com/photo-1555597673-b21d5c935865?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2FyYXRlfGVufDB8fDB8fHww"
             }
         ],
+        cart:[],
       }
     },
     computed: {
@@ -102,7 +113,15 @@ new Vue({
         },
         sortBySpaces: function() {
           this.classes.sort((a, b) => a.spaces - b.spaces);
+        },
+        addToCart(item) {
+          if (item.availableSpaces > 0) {
+            this.cart.push(item);
+            item.availableSpaces--; // Decrement available spaces
+          } else {
+            alert("Sorry, no available spaces for this activity.");
+          }
         }
-      }
+      },
   });
   
